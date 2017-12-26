97418b4472f39444316f70de5ef312261c1824dd
adrian27513 <adrian27513@gmail.com>
2017-12-26 12:34:13 -0500

    DriveControl: Created new Class to hold Movement Methods AutoMoveTest: Tested calling DriveControl Class. Kept Original Methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControl.java
@@ -1,46 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by mingch on 9/9/17.
+ * Created by mingch on 12/26/17.
  */
 
-@Autonomous(name = "AutonomousMovementTest")
-public class AutonomousMovementTest extends LinearOpMode {
+public class DriveControl{
     private DcMotor motorFrontRight;
     private DcMotor motorFrontLeft;
     private DcMotor motorBackRight;
     private DcMotor motorBackLeft;
-
-    DriveControl Move = new DriveControl();
-
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        motorFrontRight = hardwareMap.dcMotor.get("FrontRight2");
-        motorFrontLeft = hardwareMap.dcMotor.get("FrontLeft3");
-        motorBackRight = hardwareMap.dcMotor.get("BackRight0");
-        motorBackLeft = hardwareMap.dcMotor.get("BackLeft1");
-
-        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
-
-        waitForStart();
-
-        Move.drive(1,10);
-
-        fourWheelTurn(1,90);
-        sleep(500);
-        fourWheelTurn(1,180);
-        sleep(500);
-        fourWheelTurn(1,360);
-
-    }
 
     public void drive(double power, double inches) {
         power = power*0.5;
@@ -51,9 +22,9 @@ public class AutonomousMovementTest extends LinearOpMode {
         double w = (inches/23)*1000;
         int y = (int) Math.rint(w);
         String x = Integer.toString(y);
-        telemetry.addLine((power > 0 ? "Forward" : "Backward") + x + "Inches");
-        telemetry.update();
+
         sleep(y);
+
         completeStop();
     }
 
@@ -65,8 +36,6 @@ public class AutonomousMovementTest extends LinearOpMode {
         double w = (degrees/180)*1000;
         int y = (int) Math.rint(w);
         String x = Integer.toString(y);
-        telemetry.addLine((power > 0 ? "Counterclockwise" : "Clockwise") + x + "Degrees");
-        telemetry.update();
         sleep(y);
         completeStop();
     }
@@ -77,7 +46,6 @@ public class AutonomousMovementTest extends LinearOpMode {
         motorFrontLeft.setPower(power);
         motorBackRight.setPower(power);
         String x = Long.toString(time);
-        telemetry.addLine((power > 0 ? "Forwards for" : "Backwards for") + x + "seconds");
         sleep(time * 1000);
     }
 
@@ -87,4 +55,14 @@ public class AutonomousMovementTest extends LinearOpMode {
         motorFrontLeft.setPower(0);
         motorBackRight.setPower(0);
     }
+
+    public void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
 }
+
