1f63445fabd937a4d6daa82debec4ef95b0b4a90
CriticalOverload <critical.overlord@gmail.com>
2019-03-08 16:36:00 -0500

    Test Android Studio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -4,15 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-@Autonomous(name = "TouchTest")
+@Autonomous(name = "TestClass")
 
-public class TouchSensor extends LinearOpMode{
+public class TestClass extends LinearOpMode{
 
-    private DigitalChannel touchSensor;
 
     public void runOpMode() throws InterruptedException{
 
-        touchSensor = hardwareMap.digitalChannel.get("touchSensor");
 
         telemetry.addData("Status","Good");
         telemetry.update();
@@ -20,12 +18,6 @@ public class TouchSensor extends LinearOpMode{
         waitForStart();
 
         while(opModeIsActive()){
-
-            if(touchSensor.getState() == false){
-                telemetry.addData("Touch Sensor","Pressed");
-            }else{
-                telemetry.addData("Touch Sensor","Not pressed");
-            }
 
             telemetry.update();
         }
